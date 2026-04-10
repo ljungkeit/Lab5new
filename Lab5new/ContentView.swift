b42200cfef8cc06bd77309fc580e356e88624292
Luca Jungkeit <ljungkeit@vt.edu>
2026-04-10 17:34:45 -0400

    Initial Commit

---
+++ Lab5new/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab5new
+//
+//  Created by lending on 4/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab5new/Lab5newApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5newApp.swift
+//  Lab5new
+//
+//  Created by lending on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab5newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
