a34d200ad74762335be1544928e176425f93d52e
Rayan Charah <rayancharah@vt.edu>
2026-04-12 12:48:35 -0400

    Updates to creatues

--- Lab5new/Creature.swift
+++ Lab5new/Creature.swift
@@ -8,32 +8,35 @@
 import SwiftUI
 import Foundation
 
-struct Creatures {
+struct Creature {
     // Name does not need to change thus let
     let name: String
     var hunger: Int
     var energy: Int
     
-    var statusDescirption: String {
-        if (energy > 80 && hunger < 20) {
+    var statusDescription: String {
             
-            // Happy
             
-        } else if (energy < 80 && hunger < 20) {
+        if (energy < 80 && hunger < 20) {
             
             // Low Energy
+            return (name) + " is Low on Energy"
             
         } else if (energy > 80 && hunger > 20) {
             
             // Very Hungry
+            return (name) + " is Very Hungry"
             
         } else if (energy < 80 && hunger > 20) {
             
             // Very Hungry and Low Energy
+            return (name) + " is Very Hungry and Low on Energy"
             
         }
         
-        return "Finish Implementing'"
+        return (name) + " is Happy and Fed!"
+
+        
     }
     
     
